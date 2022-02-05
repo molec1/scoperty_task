78cc0da651824d37c234df428a5bf767c8e764b0
Alex <aleksandr,khablov@realtify.cz>
2022-02-05 12:02:55 +0300

    first iterations

---
+++ avm_train.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 11:03:44 2022
+
+@author: alex khablov
+"""
+
+import pandas as pd
+import numpy as np
+
+#0 read the data
+
+full_df = pd.read_csv('data/scoperty_sample_data.csv')
